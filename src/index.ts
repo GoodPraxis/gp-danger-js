8f65d20df3c92a10975b589ca7d989a43b833653
Maciek Baron <maciej@goodpraxis.uk>
2020-06-12 14:54:40 +0100

    Add package & image checks, bump version

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL
 
 declare function warn(message: string): void;
 declare function markdown(message: string): void;
+declare function message(message: string): void;
 
 declare var danger: DangerDSLType;
 
 const BIG_MR_THRESHOLD = 10;
+const LONG_TITLE_THRESHOLD = 50;
 
 const checkMergeRequest = () => {
   const {
@@ -13,6 +15,11 @@ const checkMergeRequest = () => {
     changes_count: changesCount,
     description,
   } = danger.gitlab.mr;
+
+  const {
+    modified_files: modifiedFiles,
+    created_files: createdFiles,
+  } = danger.git;
 
   /*
   * Check if description exists.
@@ -69,7 +76,7 @@ const checkMergeRequest = () => {
       warn(`Full stop in commit title found: \`${commit.title}\``);
     }
 
-    if (commit.title.length > 50) {
+    if (commit.title.length > LONG_TITLE_THRESHOLD) {
       isLongCommitFound = true;
       warn(`Long commit message found: \`${commit.title}\``);
     }
@@ -77,7 +84,8 @@ const checkMergeRequest = () => {
 
   if (isMergeCommitFound) {
     markdown(`It looks like your MR contains a merge commit. In most situations
-    this should never be the case.`);
+    this should not be the case. If there is no major reason for it to be there,
+    it's best if you squash that commit to keep a clean git history.`);
   }
 
   if (isWrongCommitFound) {
@@ -91,14 +99,37 @@ const checkMergeRequest = () => {
   }
 
   if (isLongCommitFound) {
-    markdown(`Ideally, the subject line of a commit should be 50 characters or
-    less. Additional information can be included in the body of the commit
-    message.`);
+    markdown(`Ideally, the subject line of a commit should be
+    ${LONG_TITLE_THRESHOLD} characters or less. Additional information can be
+    included in the body of the commit message if needed.`);
   }
 
   if (isWrongCommitFound || isFullStopFound || isLongCommitFound) {
     markdown(`You can edit your commit message using \`git commit --amend\` and
     then do a force push with \`git push origin HEAD -f\`.`);
+  }
+
+  /*
+   * Check package.json changes.
+   */
+  const isPackageChanged = modifiedFiles.includes('package.json');
+  const isLockfileChanged = modifiedFiles.includes('package-lock.json');
+  if (isPackageChanged && !isLockfileChanged) {
+    warn('Changes were made to `package.json`, but not to `package-lock.json`');
+    markdown(`It's unusual for \`package.json\` to change without the lock file
+    changing too. If you changed packages, make sure you run \`npm install\`.`);
+  }
+
+  /*
+   * Check created files for optimizable images.
+   */
+  const optimizableImages = createdFiles.filter(
+    (name) => name.endsWith('.png') || name.endsWith('.gif'));
+
+  if (optimizableImages.length > 0) {
+    message('Image files have been added – make sure they have been optimized');
+    markdown(`Make sure that any new images you add have been optimized using
+    tools like [ImageOptim](https://imageoptim.com/) or similar.`)
   }
 };
 
